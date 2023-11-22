16e97631d2778f9e4d687c083c5d2c8fcbeac556
Abi Ny Fiderana <hei.abigail.3@gmail.com>
2023-11-22 21:22:17 +0300

    feat: add test for getEven HTTP response expecting a even number

--- src/test/java/ac/lou/poja/endpoint/rest/controller/RandomNumberControllerIT.java
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/RandomNumberControllerIT.java
@@ -1,2 +1,17 @@
-package ac.lou.poja.endpoint.rest.controller;public class RandomNumberControllerIT {
+package ac.lou.poja.endpoint.rest.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ac.lou.poja.conf.FacadeIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class RandomNumberControllerIT extends FacadeIT {
+  @Autowired RandomNumberController controller;
+
+  @Test
+  public void evenNumberResponse() {
+    int response = controller.getEven() % 2;
+    assertEquals(0, response);
+  }
 }
