9e2f0e2e6ba1b9060e1171562a01324fd25a28ac
MiiJack <f.rama723@gmail.com>
2023-11-23 23:40:24 +0300

    chore: fixed formatting

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @Value
 public class EvenController {
-    @GetMapping("/even")
-    public int getEven() {
-        int seed = (int) (Math.random() * Integer.MAX_VALUE);
-            return seed % 2 == 0 ? seed : seed - 1 ;
-    }
+  @GetMapping("/even")
+  public int getEven() {
+    int seed = (int) (Math.random() * Integer.MAX_VALUE);
+    return seed % 2 == 0 ? seed : seed - 1;
+  }
 }
--- src/test/java/ac/lou/poja/endpoint/rest/controller/EvenControllerIT.java
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/EvenControllerIT.java
@@ -1,18 +1,17 @@
 package ac.lou.poja.endpoint.rest.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import ac.lou.poja.conf.FacadeIT;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class EvenControllerIT extends FacadeIT {
 
-    @Autowired
-    EvenController evenController;
+  @Autowired EvenController evenController;
 
-    @Test
-    void isEven() {
-        assertEquals(0, evenController.getEven() % 2);
-    }
+  @Test
+  void isEven() {
+    assertEquals(0, evenController.getEven() % 2);
+  }
 }
