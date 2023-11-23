f91c6cdf8dfe4ac4014f709cb3d3f635618194bc
Harenabs21 <hei.harena.6@gmail.com>
2023-11-23 11:54:03 +0300

    feat: add testGetEven method in HealthControllerIT

--- src/test/java/ac/lou/poja/endpoint/rest/controller/HealthControllerIT.java
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/HealthControllerIT.java
@@ -1,6 +1,7 @@
 package ac.lou.poja.endpoint.rest.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ac.lou.poja.conf.FacadeIT;
 import org.junit.jupiter.api.Test;
@@ -9,12 +10,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 class HealthControllerIT extends FacadeIT {
 
   @Autowired HealthController healthController;
-
+  @Autowired EvenController evenController;
   @Test
   void ping() {
     assertEquals("pong", healthController.ping());
   }
 
+  @Test
+  void testGetEven() {
+    int result = evenController.getEven();
+    assertTrue(result % 2 != 0, "Le résultat devrait être un nombre impair.");
+  }
   @Test
   void can_read_from_dummy_table() {
     var dummyTableEntries = healthController.dummyTable();
