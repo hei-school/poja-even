9e7535309840a86ea88d4ee3bc7e8fd54c06d287
sanda03 <hei.sanda.2@gmail.com>
2023-11-24 00:54:51 +0300

    test: create one test for /even

---
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/EvenControllerIT.java
@@ -0,0 +1,21 @@
+package ac.lou.poja.endpoint.rest.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ac.lou.poja.conf.FacadeIT;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EvenControllerIT extends FacadeIT {
+
+  @Autowired
+  RandomNumberController  randomNumberController;
+
+  @Test
+  void should_get_only_even_number() {
+    for (int i = 0; i < 30; i++) {
+      int randomEven = randomNumberController.getEvenNumber();
+      assertEquals(0, randomEven % 2);
+    }
+  }
+}
