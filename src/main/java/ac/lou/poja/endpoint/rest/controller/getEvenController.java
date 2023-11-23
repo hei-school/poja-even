f7942d091e6b485d267ac49603dcd1687b1f67c7
Antenaina2002 <hei.antenaina@gmail.com>
2023-11-24 00:09:34 +0100

    added endpoint Get /even that get random even number in HealthController.

--- src/main/java/ac/lou/poja/endpoint/rest/controller/getEvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/getEvenController.java
@@ -3,7 +3,7 @@ package ac.lou.poja.endpoint.rest.controller;
 import java.util.Random;
 
 public class getEvenController {
-    public int getRandomEvenNumber() {
+    public static int getRandomEvenNumber() {
         Random random = new Random();
         return random.nextInt(Integer.MAX_VALUE / 2) * 2;
     }
