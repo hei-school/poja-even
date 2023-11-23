6d5f7696aea26182baefadcd61e6f95c50d364ea
AndyPendragon <116746292+AndyPendragon@users.noreply.github.com>
2023-11-23 07:06:37 +0300

    chore: format the project

--- src/test/java/ac/lou/poja/endpoint/rest/controller/EvenIT.java
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/EvenIT.java
@@ -2,18 +2,17 @@ package ac.lou.poja.endpoint.rest.controller;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import ac.lou.poja.conf.FacadeIT;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import ac.lou.poja.conf.FacadeIT;
+class EvenIT extends FacadeIT {
 
-class EvenIT extends FacadeIT{
-    
-    @Autowired EvenController evenController;
+  @Autowired EvenController evenController;
 
-    @Test
-    void getEven(){
-        boolean isEven = (evenController.getEven() % 2 == 0);
-        assertTrue(isEven);
-    }
+  @Test
+  void getEven() {
+    boolean isEven = (evenController.getEven() % 2 == 0);
+    assertTrue(isEven);
+  }
 }
