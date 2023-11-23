6c7adf747607fc2d7eb21439a01995b3e208bfdc
Mii <32428593+MiiJack@users.noreply.github.com>
2023-11-23 23:02:14 +0300

    feat: added /even endpoint

---
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -0,0 +1,15 @@
+package ac.lou.poja.endpoint.rest.controller;
+
+import lombok.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Value
+public class EvenController {
+    @GetMapping("/even")
+    public int getEven() {
+        int seed = (int) (Math.random() * Integer.MAX_VALUE);
+            return seed % 2 == 0 ? seed : seed - 1 ;
+    }
+}
