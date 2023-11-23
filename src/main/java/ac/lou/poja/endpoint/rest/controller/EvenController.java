64907b21b402781bdb15401d51c2ad08d36b4746
fanambynana <hei.fanambinana@gmail.com>
2023-11-23 22:55:25 +0300

    feat: add EvenControlter and add GET /even endpoint

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -1,4 +1,19 @@
 package ac.lou.poja.endpoint.rest.controller;
 
+import ac.lou.poja.PojaGenerated;
+import ac.lou.poja.service.event.EvenService;
+import lombok.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PojaGenerated
+@RestController
+@Value
 public class EvenController {
+    EvenService evenService;
+
+    @GetMapping("/even")
+    public int genNumber() {
+        return evenService.genNumber();
+    }
 }
