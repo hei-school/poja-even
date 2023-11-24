0718498ddfd290868e4fdcc663ad68c062706dbc
RickaPrincy <rckprincy@gmail.com>
2023-11-24 07:18:46 +0300

    feat: make the random even number between MIN_VALUE and MAX_VALUE

--- src/main/java/ac/lou/poja/endpoint/rest/controller/RandomNumberController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/RandomNumberController.java
@@ -3,11 +3,14 @@ package ac.lou.poja.endpoint.rest.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
 public class RandomNumberController {
   @GetMapping("/even")
   public int getEvenNumber() {
-    int number = (int) (Integer.MAX_VALUE * Math.random());
-    return number % 2 == 0 ? number : ++number;
+    Random random = new Random();
+    int randomNumber = random.nextInt(Integer.MIN_VALUE, Integer.MAX_VALUE);
+    return (randomNumber % 2 == 0) ? randomNumber : ++randomNumber;
   }
 }
