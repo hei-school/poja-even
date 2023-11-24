ae33e0979fdcf902b364260f775e3a731382a46a
EspereJonathan <hei.espere@gmail.com>
2023-11-24 18:53:49 +0300

    chore: EvenControllerIT tested

---
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/EvenControllerIT.java
@@ -0,0 +1,16 @@
+package ac.lou.poja.endpoint.rest.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import ac.lou.poja.conf.FacadeIT;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class EvenControllerIT extends FacadeIT {
+    @Autowired EvenController evenController;
+
+    @Test
+    void gen_random_even_number() {
+        assertEquals(0, (evenController.genNumber() % 2));
+    }
+}
