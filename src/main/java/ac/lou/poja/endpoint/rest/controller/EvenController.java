87ad452d40e6c47ad2ecc61dfd2735091f7dd9ea
HarenaFiantso <fiantsorav@gmail.com>
2023-11-24 17:49:40 +0300

    fix: Fix the implementation of the /even

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -4,15 +4,18 @@ import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @RestController
 @Value
 public class EvenController {
     @GetMapping("/even")
     public int getEven() {
-        int randomValue = (int) (Math.random() * Integer.MAX_VALUE);
-        if (randomValue % 2 == 0) {
-            return randomValue;
-        }
-        return randomValue - 1;
+        return generateEvenNumber();
+    }
+
+    private int generateEvenNumber() {
+        int randomValue = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+        return randomValue % 2 == 0 ? randomValue : randomValue - 1;
     }
 }
