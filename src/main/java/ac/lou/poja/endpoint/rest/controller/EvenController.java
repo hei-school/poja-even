aed03a37d59e4d7afac6e228065926d8a8ace2a6
Lovatiana15 <hei.lovatiana@gmail.com>
2023-11-24 11:07:19 +0200

    feat: add endpoint Get/even with generate even number method

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -1,16 +1,18 @@
 package ac.lou.poja.endpoint.rest.controller;
-import ac.lou.poja.PojaGenerated;
-import java.util.Random;
-import lombok.Value;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@PojaGenerated
-@RestController
+import java.util.Random;
 
-@GetMapping("/even")
-public class EvenController {
-    Random random = new Random();
-    int even = random.nextInt(Integer.MAX_VALUE / 2) * 2; 
-    return even;
+@RestController
+public class RandomEvenController {
+
+    @GetMapping("/even")
+    public int getEven() {
+        Random random = new Random();
+        int evenNumber = random.nextInt(Integer.MAX_VALUE / 2) * 2;
+        return evenNumber;
+    }
 }
+
