5e40eed33ba2b3b1fc7ff22070eb6a041d891a08
Sullivan13 <jorosullivan13@gmail.com>
2023-11-24 09:09:17 +0300

    feat: create test and controller directory

--- src/main/java/ac/lou/poja/PojaApplication.java
+++ src/main/java/ac/lou/poja/PojaApplication.java
@@ -2,11 +2,9 @@ package ac.lou.poja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 @PojaGenerated
-public class PojaApplication {
-
+public class PojaApplication {}
   public static void main(String[] args) {
     SpringApplication.run(PojaApplication.class, args);
   }
