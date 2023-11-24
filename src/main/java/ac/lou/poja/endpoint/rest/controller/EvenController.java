f8aa1f3053bea5cc7e23e8e9efcf946264190edb
HarenaFiantso <fiantsorav@gmail.com>
2023-11-24 17:31:32 +0300

    fix: Fixing the the implementation of the /even

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -4,16 +4,15 @@ import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 @RestController
 @Value
 public class EvenController {
     @GetMapping("/even")
     public int getEven() {
-        return ThreadLocalRandom.current().ints(1, Integer.MAX_VALUE)
-                .filter(value -> value % 2 == 0)
-                .findFirst()
-                .orElseGet(() -> ThreadLocalRandom.current().nextInt(0, Integer.MAX_VALUE - 1));
+        int randomValue = (int) (Math.random() * Integer.MAX_VALUE);
+        if (randomValue % 2 == 0) {
+            return randomValue;
+        }
+        return randomValue - 1;
     }
 }
--- src/test/java/ac/lou/poja/endpoint/rest/controller/EvenControllerIT.java
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/EvenControllerIT.java
@@ -1,19 +1,22 @@
 package ac.lou.poja.endpoint.rest.controller;
 
-import ac.lou.poja.conf.FacadeIT;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-class EvenControllerIT extends FacadeIT {
-
-    @Autowired
-    protected EvenController evenController;
+@SpringBootTest
+public class EvenControllerIT {
+    @Autowired EvenController evenController;
 
     @Test
-    void getEven_returnsEvenNumber() {
-        int evenValue = evenController.getEven();
-        assertTrue(evenValue % 2 == 0, "Expected an even number, but got: " + evenValue);
+    void isEven() {
+        ArrayList<Integer> randomValues = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            randomValues.add(evenController.getEven());
+        }
+        assertEquals(0, randomValues.stream().reduce(Integer::sum).orElse(1) % 2);
     }
 }
