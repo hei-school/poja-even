07ad4f96849a044256e20151dbc86265867659a8
HarenaFiantso <fiantsorav@gmail.com>
2023-11-24 18:08:20 +0300

    refactor: Format the whole code

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -1,21 +1,20 @@
 package ac.lou.poja.endpoint.rest.controller;
 
+import java.util.concurrent.ThreadLocalRandom;
 import lombok.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ThreadLocalRandom;
-
 @RestController
 @Value
 public class EvenController {
-    @GetMapping("/even")
-    public int getEven() {
-        return generateEvenNumber();
-    }
+  @GetMapping("/even")
+  public int getEven() {
+    return generateEvenNumber();
+  }
 
-    private int generateEvenNumber() {
-        int randomValue = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
-        return randomValue % 2 == 0 ? randomValue : randomValue - 1;
-    }
+  private int generateEvenNumber() {
+    int randomValue = ThreadLocalRandom.current().nextInt(Integer.MAX_VALUE);
+    return randomValue % 2 == 0 ? randomValue : randomValue - 1;
+  }
 }
