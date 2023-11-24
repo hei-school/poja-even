ea24c28b9c73b1db61e7161928ff8d2619d82c1e
RSitrakaFL <hei.sitraka@gmail.com>
2023-11-24 23:36:57 +0300

    feat: add an endpoint and test for generate random even number

---
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/RandomEvenNumberController.java
@@ -0,0 +1,17 @@
+package ac.lou.poja.endpoint.rest.controller;
+
+import java.util.Random;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomEvenNumberController {
+    @GetMapping("/even")
+    public int getEvenNumber() {
+        Random random = new Random();
+        int evenNumber = random.nextInt(Integer.MAX_VALUE / 2) ;
+        System.out.println(evenNumber % 2 == 0);
+        return evenNumber;
+    }
+
+}
