6df08ebb0d9acfc219fe7165efb9dcfd3ce937ac
Faniry <faniryrandria08@gmail.com>
2023-11-24 19:21:15 +0300

    feat: add evenNumber fonction and test

--- src/test/java/ac/lou/poja/endpoint/rest/controller/HealthControllerIT.java
+++ src/test/java/ac/lou/poja/endpoint/rest/controller/HealthControllerIT.java
@@ -1,8 +1,10 @@
 package ac.lou.poja.endpoint.rest.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import ac.lou.poja.conf.FacadeIT;
+import ac.lou.poja.endpoint.event.gen.EvenNumber;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -20,5 +22,11 @@ class HealthControllerIT extends FacadeIT {
     var dummyTableEntries = healthController.dummyTable();
     assertEquals(1, dummyTableEntries.size());
     assertEquals("dummy-table-id-1", dummyTableEntries.get(0).getId());
+  }
+  @Test
+  void GetEvent() {
+    EvenNumber evenNumber = new EvenNumber();
+    int number = evenNumber.GetEven();
+    assertTrue(number % 2 == 0 );
   }
 }
