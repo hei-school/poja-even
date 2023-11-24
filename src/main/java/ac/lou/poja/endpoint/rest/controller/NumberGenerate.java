84c98a33efae7884fc83886f56efaeb9f56e9b18
tendryAxel <hei.tendry@gmail.com>
2023-11-24 11:43:22 +0300

    fix : set NumberGenerate.java as RestController

--- src/main/java/ac/lou/poja/endpoint/rest/controller/NumberGenerate.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/NumberGenerate.java
@@ -1,7 +1,9 @@
 package ac.lou.poja.endpoint.rest.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class NumberGenerate {
     @GetMapping("/even")
     public int generateEven(){
