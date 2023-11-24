a1851b419c0b46f686cfca64e3a5141d7878c338
Hevitriniavo <hei.tantely@gmail.com>
2023-11-24 15:13:21 +0300

    feat: added methode getEvent

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -1,4 +1,18 @@
 package ac.lou.poja.endpoint.rest.controller;
 
+import ac.lou.poja.PojaGenerated;
+import lombok.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@PojaGenerated
+@RestController
+@Value
 public class EvenController {
+
+    @GetMapping("/even")
+    public int getEven(){
+        int n =  (int)((Integer.MAX_VALUE / 2) * Math.random()) * 2;
+        return n % 2 == 0 ? n : n - 1;
+    }
 }
