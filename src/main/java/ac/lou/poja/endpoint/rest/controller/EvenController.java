e722048024d9981ea9f499309695cca6d55798a6
Kirisaki_vk <85801331+kirisaki-vk@users.noreply.github.com>
2023-11-28 17:32:22 +0300

    chore: ternary in EvenController

--- src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
+++ src/main/java/ac/lou/poja/endpoint/rest/controller/EvenController.java
@@ -5,14 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Value
 public class EvenController {
   @GetMapping("/even")
   public int getEven() {
     int randomValue = (int) (Math.random() * Integer.MAX_VALUE);
-    if (randomValue % 2 == 0) {
-      return randomValue;
-    }
-    return randomValue - 1;
-  }
+    return randomValue % 2 == 0 ? randomValue : randomValue - 1;
 }
